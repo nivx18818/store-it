df2912430191207b4e87d98d9d20fb2386f5b2cd
NguyenXuanVinh <nongami87@gmail.com>
2025-10-28 08:48:24 +0700

    feat: add Cypress for end-to-end testing
    - integrates Cypress as a dev dependency and configures it for e2e testing with recommended settings.
    - adds npm scripts to run tests in GUI and CLI modes, enabling streamlined and automated test workflows.

---
+++ cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    baseUrl: '<http://localhost:3000>',
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    video: true,
+    retries: 2,
+  },
+});
